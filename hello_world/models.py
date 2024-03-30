b81d33b3f25626d5e6129dccda996752f1732fb9
raneem-yad <eng.raneem.yad@gmail.com>
2024-03-30 16:58:43 +0100

    add comment table

--- hello_world/models.py
+++ hello_world/models.py
@@ -13,3 +13,11 @@ class Post(models.Model):
     # The auto_now_add=True means the default created time is the time of post entry.
     created_on = models.DateTimeField(auto_now_add=True)
     status = models.IntegerField(choices=STATUS, default=0)
+
+
+class Comment(models.Model):
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name="comments")
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name="commenter")
+    body = models.TextField()
+    approved = models.BooleanField(default=False)
+    created_on = models.DateTimeField(auto_now_add=True)
