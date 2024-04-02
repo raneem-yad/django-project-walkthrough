35112a2de54501659c0402a8187c9a56c367cb03
raneem-yad <eng.raneem.yad@gmail.com>
2024-04-02 17:54:38 +0200

    adding summernote to admin panel

--- my_project/urls.py
+++ my_project/urls.py
@@ -17,6 +17,8 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path("", include("hello_world.urls"), name="blog-urls"),
     path('admin/', admin.site.urls),
+    path("", include("hello_world.urls"), name="blog-urls"),
+    path('summernote/', include('django_summernote.urls')),
+
 ]
