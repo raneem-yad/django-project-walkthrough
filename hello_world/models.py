a09ef9130d677d885ce74c562820ecba5128326f
raneem-yad <eng.raneem.yad@gmail.com>
2024-04-03 11:08:08 +0200

    adding data from json file

--- hello_world/models.py
+++ hello_world/models.py
@@ -1,8 +1,9 @@
 from django.db import models
 from django.contrib.auth.models import User
 
-
 STATUS = ((0, "Draft"), (1, "Published"))
+
+
 # Create your models here.
 class Post(models.Model):
     title = models.CharField(max_length=200, unique=True)
@@ -13,6 +14,8 @@ class Post(models.Model):
     # The auto_now_add=True means the default created time is the time of post entry.
     created_on = models.DateTimeField(auto_now_add=True)
     status = models.IntegerField(choices=STATUS, default=0)
+    excerpt = models.TextField(blank=True)
+    updated_on = models.DateTimeField(auto_now=True)
 
     class Meta:
         # The - prefix on created_on indicates the posts are displayed in descending order of creation date.
@@ -25,9 +28,16 @@ class Post(models.Model):
         # which allows us to customise the string representation of the instance.
         return f"The title of this post is {self.title}"
 
+
 class Comment(models.Model):
     post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name="comments")
     author = models.ForeignKey(User, on_delete=models.CASCADE, related_name="commenter")
     body = models.TextField()
     approved = models.BooleanField(default=False)
     created_on = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ["created_on"]
+
+    def __str__(self):
+        return f"Comment {self.body} by {self.name}"
