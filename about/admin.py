dfe1ae1d3a7b4c90345306b65e93d3dfd7973044
raneem-yad <eng.raneem.yad@gmail.com>
2024-04-04 16:13:23 +0200

    add contact us page

--- about/admin.py
+++ about/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import About
+from .models import About, CollaborateRequest
 from django_summernote.admin import SummernoteModelAdmin
 
 from about.models import About
@@ -9,3 +9,9 @@ from about.models import About
 class AboutAdmin(SummernoteModelAdmin):
 
     summernote_fields = ('content',)
+
+
+@admin.register(CollaborateRequest)
+class CollaborateRequestAdmin(admin.ModelAdmin):
+
+    list_display = ('message', 'read',)
--- about/views.py
+++ about/views.py
@@ -1,15 +1,29 @@
 from django.http import HttpResponse
 from django.shortcuts import render
-
+from django.contrib import messages
 from about.models import About
+from .forms import CollaborateForm
 
 
 # Create your views here.
 def about(request):
     about = About.objects.all().order_by('-updated_on').first()
 
+    if request.method == "POST":
+        collaborate_form = CollaborateForm(data=request.POST)
+        if collaborate_form.is_valid():
+            comment = collaborate_form.save(commit=False)
+            comment.read = 0
+            comment.save()
+            messages.add_message(
+                request, messages.SUCCESS,
+                'Collaboration request received! I endeavour to respond within 2 working days.'
+            )
+
+    collaborate_form = CollaborateForm()
+
     return render(
         request,
         "about.html",
-        {"about": about},
+        {"about": about ,'collaborate_form':collaborate_form},
     )
